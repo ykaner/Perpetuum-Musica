c510fe7bc08195602fb7a911ae1d190395323f9f
klaymen-hoborgson <shachar.harshuv@gmail.com>
2018-02-26 12:09:48 +0200

    OpenItem, HistoryBack/Foreward finished

--- PerpetuumMusica/Model/DemoData.cs
+++ PerpetuumMusica/Model/DemoData.cs
@@ -4,6 +4,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace PerpetuumMusica.Model
 {
@@ -11,14 +13,14 @@ namespace PerpetuumMusica.Model
     {
         static public ObservableCollection<PlaylistItem> DemoPlaylist = new ObservableCollection<PlaylistItem>()
         {
-            new PlaylistItem(1, new Playable("The little mermaid: Original Broadway Cast Recording", new TimeSpan(1, 46, 0), 14, "Alan Menken", null)),
-            new PlaylistItem(2, new Playable("Frozen (Original Motion Picture Soundtrack)", new TimeSpan(2, 9, 0), 21, "Kristen Anderson, Robert Lopez", null)),
-            new PlaylistItem(3, new Playable("Aladdin Musical", new TimeSpan(1, 23, 0), 21, "Alan Menken", new ObservableCollection<PlaylistItem>()
+            new PlaylistItem(1, new Playable("The little mermaid: Original Broadway Cast Recording", (BitmapImage)Application.Current.Resources["littleMermaid"], new TimeSpan(1, 46, 0), 14, "Alan Menken", null)),
+            new PlaylistItem(2, new Playable("Frozen (Original Motion Picture Soundtrack)", null,  new TimeSpan(2, 9, 0), 21, "Kristen Anderson, Robert Lopez", null)),
+            new PlaylistItem(3, new Playable("Aladdin Musical", null, new TimeSpan(1, 23, 0), 21, "Alan Menken", new ObservableCollection<PlaylistItem>()
             {
-                new PlaylistItem(1, new Playable("One Jump ahead", new TimeSpan(1, 23, 0), 21, "Alan Menken", null) )
+                new PlaylistItem(1, new Playable("One Jump ahead", null,  new TimeSpan(1, 23, 0), 21, "Alan Menken", null) )
             }))
         };
 
-        static public PlaylistItem DemoItem = new PlaylistItem(new Playable("Disney", new TimeSpan(4, 38, 00), 13, "Various Artists", DemoPlaylist));
+        static public PlaylistItem DemoItem = new PlaylistItem(new Playable("Disney", new BitmapImage(new Uri(@"C:\Users\ranha\Pictures\aladdin soundtrack.png")), new TimeSpan(4, 38, 00), 13, "Various Artists", DemoPlaylist));
     }
 }
