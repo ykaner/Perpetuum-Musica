069537c747707a471c18fee1c098f126354d04f2
klaymen-hoborgson <shachar.harshuv@gmail.com>
2018-02-26 16:47:35 +0200

    progress

--- PerpetuumMusica/ViewModel/ViewModel.cs
+++ PerpetuumMusica/ViewModel/ViewModel.cs
@@ -42,12 +42,11 @@ namespace PerpetuumMusica.ViewModel
             AddMenu = new List<MenuItem>()
             {
                 new MenuItem("Track", Image("playlistIcon"), null, null, "1"),
-                new MenuItem("Playlist",Image("trackIcon"), null, new List<MenuItem>()
-                {
-                    new MenuItem("Import"),
-                    new MenuItem("Create")
-                }, "2")
-
+                    new MenuItem("Playlist",Image("trackIcon"), null, new List<MenuItem>()
+                    {
+                        new MenuItem("Import"),
+                        new MenuItem("Create")
+                    })
             };
             Toolbar = new List<MenuItem>()
             {
